780d3e16f9932db9f53ef1f0e20f980fa23a10af
erikhaandrikman <e.haandrikman@metrological.com>
2023-02-14 12:11:59 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
+// swift-tools-version:5.7.1
+
 import PackageDescription
 
 let package = Package(
-    name: "SwiftWebSocket"
+    name: "SwiftWebSocket",
+    dependencies: [
+        .package(url: "https://github.com/erikhaandrikman/SwiftWebSocket.git", from: "2.1.0")
+    ],
+    targets: [
+        .target(
+            name: "YourTargetName",
+            dependencies: ["SwiftWebSocket"]),
+        .testTarget(
+            name: "YourTargetNameTests",
+            dependencies: ["YourTargetName"]),
+    ]
 )
